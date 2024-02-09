5f0902c5e0e563075f2d86612eff2759cad9a5ca
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-02-09 22:28:03 +0200

    Populated tables with data

---
+++ bookClub/insertGenresData.sql
@@ -0,0 +1,18 @@
+USE BookClub;
+
+GO
+
+INSERT INTO GENRES (Genre)
+VALUES 
+('Classic Literature'),
+('Fantasy'),
+('Fiction'),
+('Science Fiction'),
+('Mystery'),
+('Romance'),
+('Thriller'),
+('Graphic Novel'),
+('Young Adult'),
+('Historical Fiction');
+
+GO
---
+++ bookClub/insertMeetingsData.sql
@@ -0,0 +1,13 @@
+USE BookClub;
+
+GO
+
+INSERT INTO Meetings (MeetingDate, BookID)
+VALUES 
+
+('2023-10-04', 1),
+('2023-10-11', 2),
+('2023-10-18', 3),
+('2023-10-25', 4);
+
+GO
