f7662618a737c86421197fb4c810399d4ac48212
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-02-13 21:08:51 +0200

    Added more data to the tables

--- bookClub/insertAuthorsData.sql
+++ bookClub/insertAuthorsData.sql
@@ -8,3 +8,17 @@ VALUES
 ('Joanne', 'Rowling'),
 ('Paulo', 'Coelho'),
 ('Harper', 'Lee');
+
+GO
+
+INSERT INTO Authors (FirstName, LastName)
+VALUES 
+('Jane', 'Austen'),
+('J.D.',  'Salinger'),
+('J.R.R.', 'Tolkien'),
+('Oscar', 'Wilde'),
+('Suzanne', 'Collins'),
+('Stephen', 'King'),
+('Gillian', 'Flynn');
+
+GO
