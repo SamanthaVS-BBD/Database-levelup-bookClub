d7656cfd03af72d5f07e1c2e6ad79d5b969d55a9
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-02-15 15:49:29 +0200

    DeployingProcsToDBAndFixReviewData

--- bookClub/Select.sql
+++ bookClub/Select.sql
@@ -16,8 +16,25 @@ SELECT * FROM dbo.Meetings;
 
 SELECT * FROM dbo.Attendance;
 
+
+--scalar
 SELECT dbo.CalculateAverageRating(1) AS AverageRatingForBook1;
 
+--view
 SELECT * FROM MonthlySummary;
 
+--value 
 SELECT * FROM dbo.GetMemberAttendanceRanking(2023);
+
+
+--proc
+EXECUTE [dbo].[usp_InsertBook] @book_title = 'Twilight', @author_firstname = 'Stephenie',
+                               @author_lastname = 'Meyer', @genre = 'Romance',
+                               @book_description = 'About three things I was absolutely positive. First, Edward was a vampire. Second, there was a part of him - and I didn''t know how dominant that part might be - that thirsted for my blood.And third, I was unconditionally and irrevocably in love with him.';
+GO
+
+EXECUTE [dbo].[usp_CreateMeeting] @MeetingDate = '2024-03-16', @BookID = 1;
+GO
+
+EXECUTE [dbo].[usp_InsertBookReview] @bookID = 3, @rating = 3, @memberID = 1, @comment = "Very much average, was so bored";
+GO
